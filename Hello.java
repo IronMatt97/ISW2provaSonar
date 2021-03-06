659094c0ddd6f45047e50a54eb4aabaff2da1a2e
Matteo <matti1097@gmail.com>
2021-03-06 18:58:43 +0100

    tolgo il primo smell

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
+import java.util.logging.Logger;
+import java.util.logging.Level; 
+
 public class Hello {
+	
+	static Logger logger = Logger.getLogger(Hello.class.getName());
+	
 	public static void main(String[] args) {
 
-	logger.log("Hello world");
+	logger.log(Level.INFO,"Hello world");
 
 	}
 }
